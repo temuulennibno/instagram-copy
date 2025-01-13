cc32042cc13b621bbfc9b038c67e43fc0ec6ff1c
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2025-01-13 18:02:33 +0800

    add followers and folloowing

--- src/app/[username]/page.jsx
+++ src/app/[username]/page.jsx
@@ -5,12 +5,14 @@ import { UserContext } from "../contexts/user-context";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import { MainLayout } from "../common/MainLayout";
+import { toast } from "react-toastify";
 
 const Page = () => {
   const { username } = useParams();
-  const { user: currentUser } = useContext(UserContext);
+  const { user: currentUser, accessToken } = useContext(UserContext);
   const [posts, setPosts] = useState([]);
   const [user, setUser] = useState(null);
+  const [isFollowed, setIsFollowed] = useState(false);
 
   useEffect(() => {
     axios.get("http://localhost:3333/api/users/" + username).then((res) => {
@@ -31,6 +33,34 @@ const Page = () => {
     }
   }, [user]);
 
+  useEffect(() => {
+    if (user && currentUser) {
+      const result = user.followers.some((follower) => {
+        return follower.follower === currentUser._id;
+      });
+      setIsFollowed(result);
+    }
+  }, [user, currentUser]);
+
+  const handleFollow = () => {
+    axios
+      .post(
+        "http://localhost:3333/api/users/follow",
+        {
+          userId: user._id,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      )
+      .then((res) => {
+        setIsFollowed(res.data.followed);
+        toast.success(res.data.message);
+      });
+  };
+
   if (!user) return null;
 
   const isOwner = currentUser._id === user._id;
@@ -43,7 +73,10 @@ const Page = () => {
             <Image alt="" src={user.profileUrl} width={100} height={100} className="border w-[100px] h-[100px] rounded-full object-cover" />
           </div>
           <div>
-            <h1 className="text-2xl font-bold">{user.username}</h1>
+            <h1 className="flex gap-4 text-2xl font-bold">
+              {user.username}
+              <button onClick={handleFollow}>{isFollowed ? "Unfollow" : "Follow"}</button>
+            </h1>
             {isOwner && (
               <div>
                 <button>Edit profile</button>
@@ -52,6 +85,11 @@ const Page = () => {
             <div>
               {user.fullname} <br />
               {user.bio}
+            </div>
+            <div className="flex gap-4">
+              <div>{user.posts.length} Posts</div>
+              <div>{user.followers.length} Followers</div>
+              <div>{user.followings.length} Following</div>
             </div>
           </div>
         </div>
